a4cb25a825b553e803a6ad4f97a4728995db6f27
Puja Singh <puja@Pujas-MacBook-Air.local>
2024-03-13 13:08:23 +0100

    Added cartContext and inside that added logic to add and remove cart items

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from "./components/Header";
 import Meals from "./components/Meals.jsx";
+import { CartContextProvider } from './store/CartContext.jsx'
 
+// wrapping CartContextProvider here so the all the components and its child component can have the access to conntext and its properties
 function App() {
   return (
-    <>
-    <Header/>
-    <Meals/>
-    </>
+    <CartContextProvider>
+      <Header />
+      <Meals />
+    </CartContextProvider>
   );
 }
 
---
+++ src/store/CartContext.jsx
@@ -0,0 +1,79 @@
+import { createContext, useReducer } from "react";
+
+const ContextType = createContext({
+    item: [],
+    addItem: (item) => { },
+    removeItem: (id) => { }
+});
+
+function cartReducer(state, action) {
+    if (action.type === 'ADD_ITEM') {
+        const existingCratItemIndex = state.items.findIndex(
+            (item) => item.id === action.item.id);
+
+        const updateItems = [...state.items] //List of all items present
+        if (existingCratItemIndex > -1) {
+            //If the item is already present i want to increase the quantity .
+            const existingItem = state.items[existingCratItemIndex]
+            const updatePresentItem = {
+                ...existingItem,
+                quantity: existingItem.quantity + 1
+            }
+            updateItems[existingCratItemIndex] = updatePresentItem
+
+            return { ...state, items: updateItems }
+        } else {
+            updateItems.push({ ...action.item, quantity: 1 })
+        }
+    }
+
+    if (action.type === 'REMOVE_ITEM') {
+        const existingCratItemIndex = state.items.findIndex(
+            (item) => item.id === id);
+        const existingCartItem = state.items[existingCratItemIndex]
+        const updateItems = [...state.item]
+
+        if (state.item[existingCratItemIndex] === 1) {
+            updateItems.splice(existingCratItemIndex, 1)
+        } else {
+            //If the item is already present i want to decrease the quantity .
+            const updatePresentItem = {
+                ...existingCartItem,
+                quantity: existingCartItem.quantity - 1
+            }
+            updateItems[existingCratItemIndex] = updatePresentItem;
+            return { ...state, items: updateItems }
+        }
+    }
+    return state
+}
+
+export function CartContextProvider({ children }) {
+    const [cartState, dispatchCartItem] = useReducer(cartReducer, { item: [] })
+
+    function addItem(item) {
+        dispatchCartItem({ type: 'ADD_ITEM', item })
+    }
+
+    function removeItem(id) {
+        dispatchCartItem({ type: 'REMOVE_ITEM', id })
+    }
+
+    const cartContext = {
+        items: cartState.item,
+        addItem,
+        removeItem
+    }
+
+    /** now with value={catContext} all component consuming ContextType will have access to cartContext and through that
+     *  will have access to add and remove functionality wich we added using reducer.
+     *  we will wrap the CartContextProvider component around all the components who are interested in receiving the value i.e cartContext
+     *  in our case we will wrap it in app comonent so that the componenets and all the nested componenets can have the access
+    */
+
+    return <>
+        <ContextType.Provider value={cartContext}>{children}</ContextType.Provider>
+    </>
+}
+
+export default ContextType;
